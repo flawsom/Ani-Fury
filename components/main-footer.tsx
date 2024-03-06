bb84e1e327fd8fae13a6a2c9350d28f41a4c06d3
Siba4738 <sibaprasadpanda56@gmail.com>
2024-03-06 15:31:34 +0000

    .

--- components/main-footer.tsx
+++ components/main-footer.tsx
@@ -5,11 +5,10 @@ import { buttonVariants } from "./ui/button";
 const MainFooter = () => {
   return (
     <div className="flex flex-col justify-center items-center py-4">
-        <p className="text-sm text-muted-foreground">All right reserved to it's corresponding developers.</p>
-        <p className="text-sm text-muted-foreground">© 2024 AniFury™ | Made by Siba | flawsom | vibes.him</p>
+        <p className="text-sm text-muted-foreground">All right reservers to its corresponding developer</p>
 
         <div className="flex items-center gap-x-3">
-            <a href="https://github.com/flawsom" target="_blank" className={cn(buttonVariants({ variant: "link" }))}>
+            <a href="https://github.com/Zeddxx" target="_blank" className={cn(buttonVariants({ variant: "link" }))}>
                Github <BsGithub className="ml-4" />
             </a>
         </div>
